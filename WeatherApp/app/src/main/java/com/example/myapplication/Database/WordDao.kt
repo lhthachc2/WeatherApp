a01f8869fceb04926e03e32f34cfee7e85350748
lhthachc2 <54278594+lhthachc2@users.noreply.github.com>
2020-03-06 16:31:41 +0700

    Thach

---
+++ WeatherApp/app/src/main/java/com/example/myapplication/Database/WordDao.kt
@@ -0,0 +1,25 @@
+package com.example.myapplication.Database
+
+import androidx.lifecycle.LiveData
+import androidx.room.*
+
+@Dao
+interface WordDao {
+
+    @Query("SELECT * FROM city_table LIMIT 10")
+    fun getAlphabetizedWords(): LiveData<List<CityModel>>
+
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun insert(model: CityModel)
+
+    @Query("SELECT count(*) FROM city_table ")
+    fun countRow() : Int
+
+    @Delete
+    fun delete (model: CityModel)
+
+    @Query("SELECT * FROM city_table LIMIT 1")
+    fun getone() : CityModel
+
+
+}
